e051f24825b9c27fd6ea3b2ccf4c1117e44992b8
Alan Mun <alan@Alans-MacBook-Air.local>
2025-08-04 23:28:37 -0700

    feat: add CustomerService and refactor enrichCustomerStats()
    - Implement CustomerService to encapsulate customer-related business logic.
    - Move enrichCustomerStats(Customer) from CustomerDAO to CustomerService.
    - Delegate order statistics retrieval to OrderDAO within CustomerService.

---
+++ src/main/java/com/daidaisuki/inventory/service/CustomerService.java
@@ -0,0 +1,50 @@
+package com.daidaisuki.inventory.service;
+
+import com.daidaisuki.inventory.dao.CustomerDAO;
+import com.daidaisuki.inventory.dao.OrderDAO;
+import com.daidaisuki.inventory.model.Customer;
+import com.daidaisuki.inventory.model.dto.OrderStats;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class CustomerService {
+    private final CustomerDAO customerDAO;
+    private final OrderDAO orderDAO;
+
+    public CustomerService() {
+        this.customerDAO = new CustomerDAO();
+        this.orderDAO = new OrderDAO();
+    }
+
+    public List<Customer> getAllCustomers() throws SQLException {
+        return customerDAO.getAllCustomers();
+    }
+
+    public void addCustomer(Customer customer) throws SQLException {
+        customerDAO.addCustomer(customer);
+    }
+
+    public void updateCustoemr(Customer customer) throws SQLException {
+        customerDAO.updateCustomer(customer);
+    }
+
+    public void deleteCustomer(Customer customer) throws SQLException {
+        customerDAO.deleteCustomer(0);
+    }
+    
+    public Customer getById(int customerId) throws SQLException {
+        return customerDAO.getById(customerId);
+    }
+
+    public Customer findByName(String name) throws SQLException {
+        return customerDAO.findByName(name);
+    }
+
+    public void enrichCustomerStats(Customer customer) throws SQLException {
+        OrderStats orderStats = orderDAO.getStatsForCustomer(customer.getId());
+        customer.setTotalOrders(orderStats.getTotalOrders());
+        customer.setTotalSpent(orderStats.getTotalSpent());
+        customer.setTotalDiscount(orderStats.getTotalDiscount());
+    }
+}
