4aebfcf46dc98908f6a885a0b0650f7398e974d8
Alan Mun <alan@Alans-MacBook-Air.local>
2026-02-09 19:43:28 -0800

    refactor: remove obsolete commented MVC implementation in InventoryController

--- src/main/java/com/daidaisuki/inventory/controller/view/InventoryController.java
+++ src/main/java/com/daidaisuki/inventory/controller/view/InventoryController.java
@@ -4,6 +4,7 @@ import com.daidaisuki.inventory.base.controller.BaseTableController;
 import com.daidaisuki.inventory.controller.dialog.ProductDialogController;
 import com.daidaisuki.inventory.controller.dialog.ReceiveStockDialogController;
 import com.daidaisuki.inventory.enums.DialogView;
+import com.daidaisuki.inventory.exception.DataAccessException;
 import com.daidaisuki.inventory.model.InventoryTransaction;
 import com.daidaisuki.inventory.model.Product;
 import com.daidaisuki.inventory.model.StockBatch;
@@ -258,7 +259,7 @@ public class InventoryController extends BaseTableController<Product, InventoryV
         this.viewModel.receiveStock(
             dialogViewModel.createResult(), AppSession.getInstance().getUserId());
       }
-    } catch (SQLException e) {
+    } catch (DataAccessException e) {
       this.viewModel.handleError(e);
     }
   }
@@ -269,111 +270,3 @@ public class InventoryController extends BaseTableController<Product, InventoryV
   @FXML
   private void handleProcessReturn(ActionEvent event) {}
 }
-
-/* MVC Strucuture
-public class InventoryController extends BaseTableController<Product> {
-
-  @FXML private TableView<Product> productTable;
-  @FXML private TableColumn<Product, String> nameCol;
-  @FXML private TableColumn<Product, String> categoryCol;
-  @FXML private TableColumn<Product, Integer> stockCol;
-  @FXML private TableColumn<Product, Long> priceCol;
-  @FXML private TableColumn<Product, String> availabilityCol;
-  @FXML private TableColumn<Product, Double> costCol;
-  @FXML private TableColumn<Product, Double> shippingCol;
-
-  @FXML private Button addButton;
-  @FXML private Button editButton;
-  @FXML private Button deleteButton;
-
-  private final ProductService productService;
-
-  public InventoryController() {
-    this.productService = new ProductService();
-  }
-
-  @FXML
-  public void initialize() {
-    setupColumns();
-    initializeBase(productTable, addButton, editButton, deleteButton);
-
-    // Set default sort on nameCol ascending
-    nameCol.setSortType(TableColumn.SortType.ASCENDING);
-    productTable.getSortOrder().add(nameCol);
-  }
-
-  private void setupColumns() {
-    List<Double> ratios = new ArrayList<>(Arrays.asList(0.15, 0.15, 0.1, 0.15, 0.15, 0.15, 0.15));
-    TableColumnUtils.bindColumnWidthsByRatio(productTable, ratios);
-    nameCol.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-    categoryCol.setCellValueFactory(cellData -> cellData.getValue().categoryProperty());
-    stockCol.setCellValueFactory(cellData -> cellData.getValue().stockProperty().asObject());
-    priceCol.setCellValueFactory(cellData -> cellData.getValue().priceProperty().asObject());
-    availabilityCol.setCellValueFactory(
-        cellData -> new ReadOnlyStringWrapper(cellData.getValue().isInStock() ? "Yes" : "No"));
-    costCol.setCellValueFactory(cellData -> cellData.getValue().costProperty().asObject());
-    shippingCol.setCellValueFactory(cellData -> cellData.getValue().shippingProperty().asObject());
-
-    nameCol.setCellFactory(TableCellUtils.centerAlignedStringCellFactory());
-    stockCol.setCellFactory(TableCellUtils.centerAlignedIntegerCellFactory());
-    priceCol.setCellFactory(TableCellUtils.centerAlignedPriceCellFactory());
-    categoryCol.setCellFactory(TableCellUtils.centerAlignedStringCellFactory());
-    availabilityCol.setCellFactory(TableCellUtils.centerAlignedStringCellFactory());
-    costCol.setCellFactory(TableCellUtils.centerAlignedPriceCellFactory());
-    shippingCol.setCellFactory(TableCellUtils.centerAlignedPriceCellFactory());
-  }
-
-  @Override
-  protected List<Product> fetchFromDB() throws SQLException {
-    return productService.listProducts();
-  }
-
-  @Override
-  protected Window getWindow() {
-    return FxWindowUtils.getWindow(productTable);
-  }
-
-  @Override
-  protected void addItem(Product product) throws SQLException {
-    productService.createProduct(product);
-  }
-
-  @Override
-  protected void updateItem(Product product) throws SQLException {
-    productService.updateProduct(product);
-  }
-
-  @Override
-  protected void deleteItem(Product product) throws SQLException {
-    productService.removeProduct(product.getId());
-  }
-
-  @Override
-  protected Product showDialog(Product productToEdit) {
-    return showProductDialog(productToEdit);
-  }
-
-  private Product showProductDialog(Product productToEdit) {
-    try {
-      FXMLLoader loader = ViewLoader.loadFxml(DialogView.PRODUCT_DIALOG);
-      Stage dialogStage = new Stage();
-      dialogStage.initModality(Modality.APPLICATION_MODAL);
-      dialogStage.initOwner(productTable.getScene().getWindow());
-      Scene scene = new Scene(loader.load());
-      scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
-      dialogStage.setScene(scene);
-
-      ProductDialogController controller = loader.getController();
-      controller.setDialogStage(dialogStage);
-      controller.setModel(productToEdit);
-
-      dialogStage.showAndWait();
-
-      return controller.isSaveClicked() ? controller.getModel() : null;
-    } catch (IOException e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-}
-*/
